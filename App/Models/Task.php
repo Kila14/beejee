f1e8ab9ee0feb20ac5f38faaa3defd31f83f8159
Aleksandr Kostikov <kila14@yandex.ru>
2023-12-09 18:14:46 +0300

    Add getTasks(), addTask() and getTaskStatuses() methods to \App\Models\Task class

--- App/Models/Task.php
+++ App/Models/Task.php
@@ -4,5 +4,68 @@ namespace App\Models;
 
 class Task extends Model
 {
-
+    public function getTasks(int $page = 0, int $limit = 3, string $order = 'tasks.id ASC') : array
+    {
+        $params = [
+            'page' => $page,
+            'limit' => $limit,
+            'order' => $order,
+        ];
+        
+        return $this->execute("
+            SELECT
+                tasks.user_name,
+                tasks.user_email,
+                tasks.task_description,
+                task_statuses.title AS task_status
+            FROM
+                tasks
+            LEFT JOIN
+                task_statuses
+            ON
+                tasks.task_status = task_statuses.id
+            ORDER BY
+                :order
+            LIMIT
+                :page, :limit
+            ",
+            $params
+        )->fetchAll();
+    }
+    
+    public function addTask(array $data) : bool
+    {
+        return $this->execute("
+            INSERT INTO
+                tasks
+                (
+                    user_name,
+                    user_email,
+                    task_description,
+                    task_status,
+                    task_admin_edited
+                )
+            VALUES
+                (
+                    :user_name,
+                    :user_email,
+                    :task_description,
+                    :task_status,
+                    ''
+                )
+            ",
+            $data
+        )->errorCode();
+    }
+    
+    public function getTaskStatuses() : array
+    {
+        return $this->execute("
+            SELECT
+                *
+            FROM
+                task_statuses
+            "
+        )->fetchAll();
+    }
 }
