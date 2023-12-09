3f2f32a56bd98f438c1028ffd7f18cc60d0ab957
Aleksandr Kostikov <kila14@yandex.ru>
2023-12-09 18:01:29 +0300

    Add common_functions.php

--- public_html/index.php
+++ public_html/index.php
@@ -4,5 +4,6 @@ define('ROOT_PATH', realpath(__DIR__ . '/..'));
 
 require_once ROOT_PATH . '/Autoloader.php';
 Autoloader::register();
+require_once ROOT_PATH . '/common_functions.php';
 require_once ROOT_PATH . '/router.php';
 require_once ROOT_PATH . '/routes.php';
