48d0482a95a2bf0c7a2ed9069b21ac081c3436d4
Aleksandr Kostikov <kila14@yandex.ru>
2023-12-09 18:17:11 +0300

    Add get() method to \App\Controllers\Index class and route them in routes.php

--- App/Controllers/Index.php
+++ App/Controllers/Index.php
@@ -4,5 +4,14 @@ namespace App\Controllers;
 
 class Index
 {
-
+    public function get() : void
+    {
+        $taskModel = new \App\Models\Task();
+        $tasks = $taskModel->getTasks(0, 3, 'ASC');
+        
+        if (! empty($task_add_edit_result_cookie = $_COOKIE['task_add_edit_result'] ?? ''))
+            setcookie('task_add_edit_result', '', time() - 3600);
+        
+        echo template('templates/home.php', ['tasks' => $tasks, 'task_add_edit_result_cookie' => $task_add_edit_result_cookie]);
+    }
 }
--- routes.php
+++ routes.php
@@ -1,3 +1,4 @@
 <?php
 
+get('/', '\App\Controllers\Index::get');
 any('/404','App/Controllers/404.php');
