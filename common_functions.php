13cf23496f61d39627d0121b44b1ed7a0eb1614f
Aleksandr Kostikov <kila14@yandex.ru>
2023-12-09 18:01:59 +0300

    Add 'template' function at common_functions.php

--- common_functions.php
+++ common_functions.php
@@ -1,1 +1,19 @@
 <?php
+
+if (! function_exists('template')) {
+    function template(string $template_path, array $variables = []) : string
+    {
+        $templates_root_path = ROOT_PATH . '/App/Views';
+        $template_path = $templates_root_path . '/' . $template_path;
+        
+        if (! file_exists($template_path))
+            return '';
+        
+        if (! empty($variables))
+            extract($variables);
+        
+        ob_start();
+        include $template_path;
+        return ob_get_clean();
+    }
+}
