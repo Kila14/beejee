aacfe72fa41e9eb5ef1d953f35021a4aabea3985
Aleksandr Kostikov <kila14@yandex.ru>
2023-12-09 18:56:21 +0300

    Add route to '/edit-task' at routes.php

--- routes.php
+++ routes.php
@@ -2,4 +2,5 @@
 
 get('/', '\App\Controllers\Index::get');
 any('/add-task', '\App\Controllers\Tasks::addTask');
+any('/edit-task', '\App\Controllers\Tasks::editTask');
 any('/404','App/Controllers/404.php');
