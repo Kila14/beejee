94f840a7e70631c2e13bdeeb867440589748e5cd
Aleksandr Kostikov <kila14@yandex.ru>
2023-12-10 19:36:51 +0300

    Modify routes in routes.php to use ROOT_URL const

--- routes.php
+++ routes.php
@@ -1,9 +1,9 @@
 <?php
 
-get('/', '\App\Controllers\Index::get');
-any('/add-task', '\App\Controllers\Tasks::addTask');
-any('/edit-task', '\App\Controllers\Tasks::editTask');
-any('/log-in', '\App\Controllers\Users::logIn');
-get('/log-out', '\App\Controllers\Users::logOut');
-get('/rebuild-db-tables','\App\Controllers\Users::rebuildDBTables');
-any('/404','App/Controllers/404.php');
+get(ROOT_URL, '\App\Controllers\Index::get');
+any(ROOT_URL . 'add-task', '\App\Controllers\Tasks::addTask');
+any(ROOT_URL . 'edit-task', '\App\Controllers\Tasks::editTask');
+any(ROOT_URL . 'log-in', '\App\Controllers\Users::logIn');
+get(ROOT_URL . 'log-out', '\App\Controllers\Users::logOut');
+get(ROOT_URL . 'rebuild-db-tables','\App\Controllers\Users::rebuildDBTables');
+any('404','App/Controllers/404.php');
