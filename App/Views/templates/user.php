429c4b2791cecb9d96d59e76ba57b0300fdebd68
Aleksandr Kostikov <kila14@yandex.ru>
2023-12-10 15:34:06 +0300

    Add user template view

---
+++ App/Views/templates/user.php
@@ -0,0 +1,10 @@
+<?= template('layouts/common/head.php'); ?>
+    <h1 class="mb-3 text-center"><?= $h1 ?? ''; ?></h1>
+    <?php if ($mode === 'log_in') : ?>
+        <div class="row justify-content-center">
+            <div class="col-12 col-sm-10 col-md-6 col-lg-4">
+                <?= template('layouts/forms/user-log-in.php', ['data' => $data, 'errors' => $errors]); ?>
+            </div>
+        </div>
+    <?php endif; ?>
+<?= template('layouts/common/footer.php'); ?>
