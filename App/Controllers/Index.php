008d6c160fd271af4e3993550e5422a0c4717d05
Aleksandr Kostikov <kila14@yandex.ru>
2023-12-10 11:55:57 +0300

    Add sort params to \App\Controllers\Index::get() method

--- App/Controllers/Index.php
+++ App/Controllers/Index.php
@@ -7,7 +7,13 @@ class Index
     public function get() : void
     {
         $taskModel = new \App\Models\Task();
-        $tasks = $taskModel->getTasks(0, 3, 'ASC');
+        $tasks_count = $taskModel->getTasksCount();
+        $page = $_GET['page'] ?? 1;
+        $count = $_GET['count'] ?? 3;
+        $offset = ($page * $count) - $count;
+        $order_field = $_GET['order-field'] ?? '';
+        $order_direction = $_GET['order-direction'] ?? '';
+        $tasks = $taskModel->getTasks((int) $offset, (int) $count, $order_field, $order_direction);
         
         if (! empty($task_add_edit_result_cookie = $_COOKIE['task_add_edit_result'] ?? ''))
             setcookie('task_add_edit_result', '', time() - 3600);
