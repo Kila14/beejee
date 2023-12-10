6821d3916f2f1814d91a0dc937f09148a317a739
Aleksandr Kostikov <kila14@yandex.ru>
2023-12-10 17:22:01 +0300

    Add '/rebuild-db-tables' route to routes.php

--- routes.php
+++ routes.php
@@ -4,5 +4,6 @@ get('/', '\App\Controllers\Index::get');
 any('/add-task', '\App\Controllers\Tasks::addTask');
 any('/edit-task', '\App\Controllers\Tasks::editTask');
 any('/log-in', '\App\Controllers\Users::logIn');
-any('/log-out', '\App\Controllers\Users::logOut');
+get('/log-out', '\App\Controllers\Users::logOut');
+get('/rebuild-db-tables','\App\Controllers\Users::rebuildDBTables');
 any('/404','App/Controllers/404.php');
