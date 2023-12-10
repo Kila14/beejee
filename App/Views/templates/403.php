503290e2b5b130c13afa808c118b33935fce0c9d
Aleksandr Kostikov <kila14@yandex.ru>
2023-12-10 16:34:14 +0300

    Add 403 template view

---
+++ App/Views/templates/403.php
@@ -0,0 +1,7 @@
+<?= template('layouts/common/head.php'); ?>
+    <h1 class="mb-3 text-center">Доступ запрещён</h1>
+    <div class="d-flex justify-content-center">
+        <a class="mx-2 btn btn-secondary" href="/">На главную</a>
+        <a class="mx-2 btn btn-primary" href="/log-in">Авторизоваться</a>
+    </div>
+<?= template('layouts/common/footer.php'); ?>
