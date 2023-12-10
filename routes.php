5f0f3875327548daa016cee2decb7e87d4ce789a
Aleksandr Kostikov <kila14@yandex.ru>
2023-12-10 15:38:10 +0300

    Add '/log-in' and '/log-out' routes to routes.php

--- routes.php
+++ routes.php
@@ -3,4 +3,6 @@
 get('/', '\App\Controllers\Index::get');
 any('/add-task', '\App\Controllers\Tasks::addTask');
 any('/edit-task', '\App\Controllers\Tasks::editTask');
+any('/log-in', '\App\Controllers\Users::logIn');
+any('/log-out', '\App\Controllers\Users::logOut');
 any('/404','App/Controllers/404.php');
